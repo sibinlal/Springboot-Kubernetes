b91be8dfc43a9eb0e97b3b662e87dba6d7a4665a
sibinlalpv <sbnlalpnr@gmail.com>
2022-09-30 13:23:26 +0530

    initial commit

---
+++ src/test/java/com/icodes/BookmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
